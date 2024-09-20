15405640892b0d36b48163fe1f9ac43477443b63
ferndz <nafernandez@mi.unc.edu.ar>
2024-09-20 20:50:03 -0300

    SCRUM-30 añadido: validaciones al formulario

--- src/components/CreateGame.jsx
+++ src/components/CreateGame.jsx
@@ -27,22 +27,22 @@ export default function CreateGame({ onCreateGame }) {
       <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formUsername">
           <Form.Label>Usuario</Form.Label>
-          <Form.Control type="text" name="submitted-username" placeholder="Ingresa tu usuario" />
+          <Form.Control required type="text" minlength="3" maxlength="20" name="submitted-username" placeholder="Ingresa tu usuario" />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formGameTitle">
           <Form.Label>Nombre de la partida</Form.Label>
-          <Form.Control type="text" name="submitted-gametitle" placeholder="Ingresa el nombre de la partida" />
+          <Form.Control required type="text" minlength="3" maxlength="20" name="submitted-gametitle" placeholder="Ingresa el nombre de la partida" />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formMinPlayers">
           <Form.Label>Cantidad minima de jugadores</Form.Label>
-          <Form.Control type="text" name="submitted-gametitle" placeholder="Mínimo" />
+          <Form.Control required type="number" min="2" max="4" name="submitted-gametitle" placeholder="Mínimo" />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formMaxPlayer">
           <Form.Label>Cantidad máxima de jugadores</Form.Label>
-          <Form.Control type="text" name="submitted-gametitle" placeholder="Máximo" />
+          <Form.Control required type="number" min="2" max="4" name="submitted-gametitle" placeholder="Máximo" />
         </Form.Group>
 
         <Button variant="primary" type="submit">
