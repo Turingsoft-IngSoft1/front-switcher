848043edc5dde8d927c1410655dafbb5e7acf2a1
AguilaMolinera <cesdarne@gmail.com>
2024-09-21 12:26:37 -0300

    SCRUM-2 Hecha la lista de partidas, lista a partir de un Json >Falta: > - Integrar con base de datos > - Poder seleccionar partidas > - Tamaño variable de los th y td que no ocupen mucho espacio (jugadores y min-max)

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
 import '../styles/App.css'
+import Match from './listMatches/containers/list.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
   return (
     <>
-      <h1>Bienvenido a El Switcher</h1>
       <div>
-        <p>
-          Juego en desarrollo....
-        </p>
+      <h1>List Dev</h1>
+      <Match />
       </div>
     </>
   )
