f7fdd3c1f1a31a8c12dbadb389e1e682d154e214
AguilaMolinera <cesdarne@gmail.com>
2024-09-26 20:27:05 -0300

    Cambios SWITCHER-53 boton con endpoint del backend para testear (no funciona)

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -2,16 +2,44 @@ import { useState, useEffect } from 'react'
 import '../styles/App.css'
 import JoinButton from './components/JoinButton'
 function App() {
-  const [Match, setMatch] = useState(null)
+  const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const [selectedMatch, setSelectedMatch] = useState(null);
+
+    const fetchData = () => {
+        fetch('http://localhost:8000/list_games')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => setData(data))
+            .catch(error => {
+                console.error('There has been a problem with your fetch operation:', error);
+                setError(error);
+            });
+    };
+
   useEffect(() => {
-    setMatch({id:12});
+    fetchData();
   }, []);
+
   return (
     <>
       <h1>Join Button test</h1>
       <div>
         <p>
-          <JoinButton selectedMatch={Match} />
+          {data ? (
+            data.map((match, index) => (
+              <div key={index}>
+                <span>{match}</span>
+                <JoinButton selectedMatch={match} />
+              </div>
+            ))
+          ) : (
+            <span>Loading...</span>
+          )}
         </p>
       </div>
     </>
