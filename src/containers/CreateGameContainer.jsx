d44bd1d4573ecf2a58aa6016ac8872fa13719b57
ferndz <nafernandez@mi.unc.edu.ar>
2024-09-27 18:51:13 -0300

    SWITCHER-78 Creacion de partida actualiza el estado a partir del contexto

--- src/containers/CreateGameContainer.jsx
+++ src/containers/CreateGameContainer.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import CreateGame from '../components/CreateGame.jsx';
+import { GameContext } from '../contexts/GameContext.jsx';
 
 export default function CreateGameContainer({ onCreateGame }) {
-
+  const { idGame, idPlayer, players, fase, setIdGame, setIdPlayer, setPlayers, setFase} = useContext(GameContext);
   const createGame = async (gameData) => {
     try {
         //TODO: direccion de api
@@ -19,16 +20,25 @@ export default function CreateGameContainer({ onCreateGame }) {
       }
 
       const responseData = await response.json();
-      const nuevoId = responseData.id;
-      onCreateGame(nuevoId);
+      const newIdGame = responseData.id_game;
+      const newIdPlayer = responseData.id_player;
+      setIdGame(newIdGame);
+      setIdPlayer(newIdPlayer);
+      setPlayers([newIdPlayer]);
+      setFase('lobby');
+      console.log('Partida creada con exito')
     } catch (error) {
       console.error('Error:', error);
     }
   };
 
-  const createGameMock = (gameData) => {
-    const idMock = 42;
-    onCreateGame(idMock);
+  const createGameMock = () => {
+    setIdGame(111);
+    setIdPlayer(42);
+    setPlayers([42]);
+    setFase('lobby');
+    console.log('Partida (mockeada) creada con exito')
   }
-  return <CreateGame onCreateGame={createGame} />;
+
+  return <CreateGame onCreateGame={createGameMock} />;
 }
