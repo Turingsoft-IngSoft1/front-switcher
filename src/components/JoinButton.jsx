333c2bcd800ae4d4d10747f52d4ab589b87454f1
AguilaMolinera <cesdarne@gmail.com>
2024-09-28 23:52:43 -0300

    SWITCHER-53 Cambios para probar

--- src/components/JoinButton.jsx
+++ src/components/JoinButton.jsx
@@ -1,17 +1,69 @@
 import React from 'react'
-import { Button } from 'react-bootstrap';
+import { Button, Modal} from 'react-bootstrap';
+import { GameContext, GameProvider } from '../contexts/GameContext.jsx';
+import { useState, useContext } from 'react';
 
 function JoinButton ({selectedMatch}) {
+    const { idGame, idPlayer, players, fase, setIdGame, setIdPlayer, setPlayers, setFase} = useContext(GameContext);
+    const [showModal, setShowModal] = useState(false);
+    const [nickname, setNickname] = useState('');
+
     const handleJoinRequest = () => {
         if (selectedMatch) {
-            // Send join request logic here
-            console.log(`Joining match: ${selectedMatch.id}`);
+            setShowModal(true);
+            console.log(`Trying to join match: ${selectedMatch.id}`);
         }
     };
+
+    const handleClose = () => setShowModal(false);
+
+    const handleSubmit = () => {
+        if (nickname.trim()) {
+            joinGame();
+            setShowModal(false);
+        }
+    };
+    const joinGame = async () => {
+        try {
+            const response = await fetch('http://127.0.0.1:8000/join_game', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                id_game: selectedMatch.id,
+                player_name: nickname
+            }),
+          });
+    
+          if (!response.ok) {
+            throw new Error('Error al crear la partida');
+          }
+    
+          const responseData = await response.json();
+          const newIdGame = responseData.id_game;
+          const newIdPlayer = responseData.id_player;
+          setIdGame(newIdGame);
+          setIdPlayer(newIdPlayer);
+          setFase('lobby');
+          console.log(`Joined: ${selectedMatch.id}`)
+        } catch (error) {
+          console.error('Error:', error);
+        }
+      };þ
+
     return (
-        <Button variant='dark' onClick={handleJoinRequest} disabled={!selectedMatch}>
-            Unirse a la sala
-        </Button>
+        <div>
+            <Button variant='dark' onClick={handleJoinRequest} disabled={!selectedMatch}>
+                Unirse a la sala
+            </Button>
+            <Modal show={showModal} onHide={handleClose}>
+                <h2> Escriba un nickname para usar </h2>
+                <input type="text" value={nickname} onChange={(e) => setNickname(e.target.value)}/>
+                <Button onClick={handleClose}>Cerrar</Button>
+                <Button onClick={handleSubmit}>Aceptar</Button>
+            </Modal>
+        </div>
     );
 }
 
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,31 +1,30 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
 import '../styles/App.css'
 import JoinButton from '../components/JoinButton'
+import { GameContext, GameProvider } from '../contexts/GameContext.jsx';
+
 function App() {
   const [data, setData] = useState(null);
-    const [error, setError] = useState(null);
-    const [selectedMatch, setSelectedMatch] = useState(null);
 
-    const fetchData = () => {
-      fetch('http://127.0.0.1:8000/list_games', {
-          headers: {
-              'accept': 'application/json'
-          }
-      })
-      .then(response => {
-          if (!response.ok) {
-              throw new Error('Network response was not ok');
-          }
-          return response.json();
-      })
-      .then(data => {
-          // Assuming setData expects the games_list array
-          setData(data.games_list);
-      })
-      .catch(error => {
-          console.error('There has been a problem with your fetch operation:', error);
-          setError(error);
-      });
+  const fetchData = () => {
+    fetch('http://127.0.0.1:8000/list_games', {
+        headers: {
+            'accept': 'application/json'
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        // Assuming setData expects the games_list array
+        setData(data.games_list);
+    })
+    .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+    });
   };
 
   useEffect(() => {
