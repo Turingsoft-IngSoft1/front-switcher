b7bb9e9387b57924b66f4ad70c7cbe945dfd0c60
Ignacio <ignas.gomezb12@gmail.com>
2024-09-29 18:47:32 -0300

    estado de los botones, el jugador que no esta en turno no puede realizar acciones

--- src/components/ButtonSet.jsx
+++ src/components/ButtonSet.jsx
@@ -1,27 +1,65 @@
+import { useContext, useState } from "react";
 import { Row, Col, Button } from "react-bootstrap";
+import {GameContext} from '../contexts/GameContext';
 
-function PedirCartasButton (){
-    return (
-        <Col>
-        <Button className="pedir-cartas-button">Pedir cartas</Button>
-        </Col>
-    )
+function PedirCartasButton ({status}){
+    switch(status){
+        case 'disabled':
+            return (
+                <Col>
+                <Button className="pedir-cartas-button" disabled>Pedir cartas</Button>
+                </Col>
+            );
+        case 'enabled':
+            return (
+                <Col>
+                <Button className="pedir-cartas-button">Pedir cartas</Button>
+                </Col>
+            );
+        default:
+            return null;
+    }
+
 }
 
-function ConfirmMovementButton (){
-    return (
-        <Col>
-        <Button className="confirm-movement-button">Confirmar movimiento</Button>
-        </Col>
-    )
+function ConfirmMovementButton ({status}){
+    switch(status){
+        case 'disabled' :
+            return (
+                <Col>
+                <Button className="confirm-movement-button" disabled>Confirmar movimiento</Button>
+                </Col>
+            );
+        case 'enabled' :
+            return(
+                <Col>
+                <Button className="confirm-movement-button">Confirmar movimiento</Button>
+                </Col>
+            );
+        default:
+            return null;
+    }
+
 }
 
-function NextTurnButton (){
-    return (
-        <Col>
-        <Button className="next-turn-button">Siguiente turno</Button>
-        </Col>
-    )
+function NextTurnButton ({onNextTurnClick, status}){
+    switch(status){
+        case 'disabled':
+            return (
+                <Col>
+                <Button className="next-turn-button" onClick={onNextTurnClick} disabled>Siguiente turno</Button>
+                </Col>
+            );
+        case 'enabled':
+            return (
+                <Col>
+                <Button className="next-turn-button" onClick={onNextTurnClick}>Siguiente turno</Button>
+                </Col>
+            );
+        default:
+            return null;
+    }
+
 }
 
 function LeaveGameButton () {
@@ -32,13 +70,26 @@ function LeaveGameButton () {
     );
 }
 
-export default function ButtonSet () {
-    return (
-        <Row className="justify-content-md-around p-3">
-            <PedirCartasButton/>
-            <ConfirmMovementButton/>
-            <NextTurnButton/>
-            <LeaveGameButton />
-        </Row>     
+export default function ButtonSet ({handleNextTurnClick}) {
+    const {turnPlayer, idPlayer} = useContext(GameContext);
+    if (turnPlayer == idPlayer){
+        return (
+            <Row className="justify-content-md-around p-3">
+                <PedirCartasButton status={'enabled'}/>
+                <ConfirmMovementButton status={'enabled'}/>
+                <NextTurnButton onClick = {handleNextTurnClick} status={'enabled'}/>
+                <LeaveGameButton />
+            </Row>     
         );
+    }
+    else {
+        return (
+            <Row className="justify-content-md-around p-3">
+                <PedirCartasButton status={'disabled'}/>
+                <ConfirmMovementButton status={'disabled'}/>
+                <NextTurnButton onClick = {handleNextTurnClick} status={'disabled'}/>
+                <LeaveGameButton />
+            </Row>      
+        );
+    }
 }
