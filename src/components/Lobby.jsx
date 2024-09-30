875e517d51f3dc7c72674c6f9bee4cdd242a83f8
AguilaMolinera <cesdarne@gmail.com>
2024-09-30 12:03:59 -0300

    SWITCHER-87 Cambio de codigo estetico pequeño en Lobby.jsx

--- src/components/Lobby.jsx
+++ src/components/Lobby.jsx
@@ -104,43 +104,43 @@ export default function Lobby (){
         <Row>
             <Col xs="11">
                 {/* Cartas del jugador 1, tablero jugador 3 */}
-                                <Row>
-                                    <Col xs="12" className="justify-content-md-center">
-                                    {players.length > 1 && (() => {
-                                        const otherPlayers = players.filter(player => player !== idPlayer);
-                                        const firstPlayer = otherPlayers[0]; // Get the second player in the list
-                                        console.log(firstPlayer)
-                                        return firstPlayer ? (
-                                            <h1>{playersNames[players.indexOf(firstPlayer)]}</h1>
-                                        ) : null;
-                                    })()}
-                                    </Col>
-                                </Row>
-                                <Row>
-                                    <Col xs="auto" className="justify-content-md-left">
-                                        {players.length > 2 && (() => {
-                                            const otherPlayers = players.filter(player => player !== idPlayer);
-                                            const secondPlayer = otherPlayers[1]; // Get the second player in the list
-                                            console.log(secondPlayer);
-                                            return secondPlayer ? (
-                                                <h1>{playersNames[players.indexOf(secondPlayer)]}</h1>
-                                            ) : null;
-                                        })()}
-                                    </Col>
-                                    <Col xs="auto">
-                                        <Board />
-                                    </Col>
-                                    <Col xs="auto" className="justify-content-md-right">
-                                        {players.length > 3 && (() => {
-                                            const otherPlayers = players.filter(player => player !== idPlayer);
-                                            const thirdPlayer = otherPlayers[2]; // Get the second player in the list
-                                            console.log(thirdPlayer);
-                                            return thirdPlayer ? (
-                                                <h1>{playersNames[players.indexOf(thirdPlayer)]}</h1>
-                                            ) : null;
-                                        })()}
-                                    </Col>
-                                </Row>
+                <Row>
+                    <Col xs="12" className="justify-content-md-center">
+                    {players.length > 1 && (() => {
+                        const otherPlayers = players.filter(player => player !== idPlayer);
+                        const firstPlayer = otherPlayers[0]; // Get the second player in the list
+                        console.log(firstPlayer)
+                        return firstPlayer ? (
+                            <h1>{playersNames[players.indexOf(firstPlayer)]}</h1>
+                        ) : null;
+                    })()}
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs="auto" className="justify-content-md-left">
+                        {players.length > 2 && (() => {
+                            const otherPlayers = players.filter(player => player !== idPlayer);
+                            const secondPlayer = otherPlayers[1]; // Get the second player in the list
+                            console.log(secondPlayer);
+                            return secondPlayer ? (
+                                <h1>{playersNames[players.indexOf(secondPlayer)]}</h1>
+                            ) : null;
+                        })()}
+                    </Col>
+                    <Col xs="auto">
+                        <Board />
+                    </Col>
+                    <Col xs="auto" className="justify-content-md-right">
+                        {players.length > 3 && (() => {
+                            const otherPlayers = players.filter(player => player !== idPlayer);
+                            const thirdPlayer = otherPlayers[2]; // Get the second player in the list
+                            console.log(thirdPlayer);
+                            return thirdPlayer ? (
+                                <h1>{playersNames[players.indexOf(thirdPlayer)]}</h1>
+                            ) : null;
+                        })()}
+                    </Col>
+                </Row>
 
                                 {/* Cartas del jugador usuario (0) */}
                 <Row className="justify-content-md-center">
