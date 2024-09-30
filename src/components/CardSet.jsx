71e0a55f0addf8659979282cdc12c66410a1673a
nafernandez302 <nafernandez@mi.unc.edu.ar>
2024-09-30 14:17:31 -0300

    SWITCHER-91 cardset estilizado

--- src/components/CardSet.jsx
+++ src/components/CardSet.jsx
@@ -4,9 +4,9 @@ import { Card, Container, Button, Row, Col } from "react-bootstrap";
 export default function CardSet (){
     return (
         <>
-        <Col xs="auto" className="carta"><Card style={{ width: '60px', height: '90px' }}></Card></Col>
-        <Col xs="auto" className="carta"><Card style={{ width: '60px', height: '90px' }}></Card></Col>
-        <Col xs ="auto" className="carta"><Card style={{ width: '60px', height: '90px' }}></Card></Col>
+        <Col xs="auto" className="cardset"><Card className="card" style={{ width: '60px', height: '90px' }}></Card></Col>
+        <Col xs="auto" className="cardset"><Card className="card" style={{ width: '60px', height: '90px' }}></Card></Col>
+        <Col xs ="auto" className="cardset"><Card className="card" style={{ width: '60px', height: '90px' }}></Card></Col>
         </>
     );
 }
