c77add86e46d9b524a1b8bed3c935c848c6c20c8
nafernandez302 <nafernandez@mi.unc.edu.ar>
2024-09-30 14:09:16 -0300

    SWITCHER-91 Estructura mejorada de Game

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,17 +1,18 @@
 import { useEffect, useContext } from "react";
-import { Row, Col, Button } from "react-bootstrap";
+import { Card, Row, Col, Button } from "react-bootstrap";
 import '../styles/Board.css';
 import { GameContext } from '../contexts/GameContext.jsx';
 import Board from './Board.jsx';
-import ButtonSet from  './ButtonSet.jsx'
+import ButtonSet from './ButtonSet.jsx'
+import CardSet from './CardSet.jsx'
 
 
-export default function Game({onPassTurn}) {
+export default function Game({ onPassTurn }) {
     const { setBoard } = useContext(GameContext);
 
 
     useEffect(() => {
-        const initialBoard = Array(36).fill("dark"); 
+        const initialBoard = Array(36).fill("dark");
 
         // alternacion entre "danger" y "success" en el tablero
         for (let i = 0; i < 36; i++) {
@@ -22,9 +23,45 @@ export default function Game({onPassTurn}) {
     }, [setBoard]);
 
     return (
-        <Row>
-                <Board />
+        <>
+            <Row>
+                <Col xs={4} md={3} className="d-flex align-items-center">
+                    <h4>Vacio</h4>
+                </Col>
+                <Col xs={4} md={6} className="d-flex align-items-center justify-content-center">
+                    <h4>Nombre Turno3</h4>
+                </Col>
+                <Col xs={4} md={3} className="d-flex align-items-center" >
+                    <h4>Vacio</h4>
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={4} md={3} className="d-flex align-items-center">
+                    <h4>NombreTurno4</h4>
+                </Col>
+                <Col xs={4} md={6}>
+                    <Board />
+                </Col>
+                <Col xs={4} md={3} className="d-flex align-items-center" >
+                    <h4>NombreTurno2</h4>
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={4} md={3} className="d-flex align-items-center">
+                </Col>
+                <Col xs={4} md={6} className="d-flex align-items-center justify-content-center">
+                    <h4>MiNombre</h4>
+                </Col>
+                <Col xs={4} md={3} className="d-flex align-items-center" >
+                </Col>
+            </Row>
+            <Row className="justify-content-md-center">
+                <CardSet />
+            </Row>
+            <Row>
                 <ButtonSet onPassTurn={onPassTurn} />
-        </Row>
+            </Row>
+        </>
+
     );
 }
