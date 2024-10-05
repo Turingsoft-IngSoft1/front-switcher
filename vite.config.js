704fe1fd6550bd4650044d012656d6ec6e598e62
nafernandez302 <nafernandez@mi.unc.edu.ar>
2024-10-05 15:10:32 -0300

    SWITCHER-122 creada variable de entorno isMock

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: '/src/setupTests.js'
-  }
-})
+export default defineConfig(({ mode }) => {
+  const useMock = mode === 'mock';
+
+  return {
+    plugins: [react()],
+    define: {
+      'import.meta.env.USE_MOCK': JSON.stringify(useMock),
+    },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: '/src/setupTests.js'
+    }
+  };
+});
