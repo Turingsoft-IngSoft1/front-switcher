ac87d2fc9047faec0f9f6bb9457f7fcd141d23c4
nafernandez302 <nafernandez@mi.unc.edu.ar>
2024-10-07 18:49:46 -0300

    imports de .svg de figuras y advertencia

--- src/components/CardSet.jsx
+++ src/components/CardSet.jsx
@@ -3,11 +3,36 @@ import {Image, Container, Col, Row} from 'react-bootstrap';
 import imgtest from '../styles/cards/fig01.svg'
 import { useContext } from "react";
 import { GameContext } from '../contexts/GameContext.jsx';
+import React from 'react';
+import Fig01 from '../styles/cards/fig01.svg';
+import Fig02 from '../styles/cards/fig02.svg';
+import Fig03 from '../styles/cards/fig03.svg';
+import Fig04 from '../styles/cards/fig04.svg';
+import Fig05 from '../styles/cards/fig05.svg';
+import Fig06 from '../styles/cards/fig06.svg';
+import Fig07 from '../styles/cards/fig07.svg';
+import Fig08 from '../styles/cards/fig08.svg';
+import Fig09 from '../styles/cards/fig09.svg';
+import Fig10 from '../styles/cards/fig10.svg';
+import Fig11 from '../styles/cards/fig11.svg';
+import Fig12 from '../styles/cards/fig12.svg';
+import Fig13 from '../styles/cards/fig13.svg';
+import Fig14 from '../styles/cards/fig14.svg';
+import Fig15 from '../styles/cards/fig15.svg';
+import Fig16 from '../styles/cards/fig16.svg';
+import Fig17 from '../styles/cards/fig17.svg';
+import Fig18 from '../styles/cards/fig18.svg';
+import Fige01 from '../styles/cards/fige01.svg';
+import Fige02 from '../styles/cards/fige02.svg';
+import Fige03 from '../styles/cards/fige03.svg';
+import Fige04 from '../styles/cards/fige04.svg';
+import Fige05 from '../styles/cards/fige05.svg';
+import Fige06 from '../styles/cards/fige06.svg';
+import Fige07 from '../styles/cards/fige07.svg';
 
-
-function CardSwitcher ({imgsource}) {
+function CardSwitcher ({imgsource, selected}) {
     return (
-        <Container className="card-switcher">
+        <Container onClick=className="card-switcher">
             <Row>
                 <Image src={imgsource} className="img-content"/>
             </Row>
@@ -15,7 +40,8 @@ function CardSwitcher ({imgsource}) {
     );
 }
 
-
+// ADVERTENCIA: Duplicacion de codigo
+// si se cambia Horizontal, se cambia Vertical
 export function CardSetHorizontal ({turn}) {
     const { playersTurns} = useContext(GameContext);
     const desiredPlayer = playersTurns.filter(player => player.turn === turn);
@@ -27,21 +53,24 @@ export function CardSetHorizontal ({turn}) {
             </Row>
             <Row className= "justify-content-md-center bg-cardset" >
                 <Col xs="auto" className="carta">
-                    <CardSwitcher imgsource={imgtest}/>
+                    <CardSwitcher imgsource={Fig01} />
                 </Col>
 
                 <Col xs="auto" className="carta">
-                    <CardSwitcher imgsource={imgtest}/>
+                    <CardSwitcher imgsource={Fig02}/>
                 </Col>
 
                 <Col xs="auto" className="carta">
-                    <CardSwitcher imgsource={imgtest}/>
+                    <CardSwitcher imgsource={Fig03}/>
                 </Col>
             </Row>
         </Col>
         </Row>
     );
 }
+
+// ADVERTENCIA: Duplicacion de codigo
+// si se cambia Horizontal, se cambia Vertical
 
 export function CardSetVertical ({turn}) {
     const { playersTurns} = useContext(GameContext);
