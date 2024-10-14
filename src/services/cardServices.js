244be805a75477005bfaf2c0445b3d991e563045
nafernandez302 <nafernandez@mi.unc.edu.ar>
2024-10-14 11:24:57 -0300

    nuevo: endpoint getFigureCards

--- src/services/cardServices.js
+++ src/services/cardServices.js
@@ -20,4 +20,27 @@ async function getMovementCards(idGame, idPlayer) {
     }
 }
 
+// Obtener JSON de las cartas de mov dado un game y un player
+export async function getFigureCards(idGame, idPlayer) {
+    try {
+        const response = await fetch(`http://127.0.0.1:8000/get_figure/${idGame}/${idPlayer}`, {
+            method: 'POST',
+            headers: {
+                'accept': 'application/json'
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error fetching movement cards:", error);
+        return null;
+    }
+}
+
+
 export default getMovementCards;
