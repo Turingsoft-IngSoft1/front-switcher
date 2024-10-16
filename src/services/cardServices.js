d247fe87521bd026cdefed5862c5db1cfcc280e1
Ignacio <ignas.gomezb12@gmail.com>
2024-10-15 23:19:22 -0300

    movement API call

--- src/services/cardServices.js
+++ src/services/cardServices.js
@@ -1,5 +1,5 @@
 // Obtener JSON de las cartas de mov dado un game y un player
-async function getMovementCards(idGame, idPlayer) {
+export async function getMovementCards(idGame, idPlayer) {
     try {
         const response = await fetch(`http://127.0.0.1:8000/get_moves/${idGame}/${idPlayer}`, {
             method: 'POST',
@@ -20,4 +20,22 @@ async function getMovementCards(idGame, idPlayer) {
     }
 }
 
-export default getMovementCards;
+export async function useMovementCard (movementData) {
+    try {
+        const response = await fetch(`http://127.0.0.1:8000/use_moves`,
+            {
+                method: 'POST',
+                headers: {'accept' : 'application/json'},
+                body: JSON.stringify(movementData),
+            }
+        );
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+    }
+    catch (error) {
+        console.error("Error using movement: ", error);
+        return null;
+    }
+}
