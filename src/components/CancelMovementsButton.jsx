3fbd0a01da66b45ba622e6c41f10fc5a6dd5dd72
nafernandez302 <nafernandez@mi.unc.edu.ar>
2024-10-21 19:10:11 -0300

    cancel game completed

--- src/components/CancelMovementsButton.jsx
+++ src/components/CancelMovementsButton.jsx
@@ -5,32 +5,48 @@ import { WebSocketContext } from "../contexts/WebSocketContext.jsx";
 import { cancelMovements } from "../utils/gameServices.js";
 
 
-function rollBackPlayedCards(idGame, idPlayer) {
-    console.log("VOLVIENDO ATRAs...");
-    cancelMovements(idGame, idPlayer);
-    
-    // TODO:
-    // 1) Actualizar las movCards con estado 'played' a 'noplayed'
-    //
-    // map(mvc.status => 'noplayed')
-    //
-    // 2) "Refrescar tablero" es automatico ya que se recibe REFRESH_BOARD por ws
 
-}
-
-function hideButton(status, cardsPlayed){
-    
-    const isDisabled = status === 'disabled';
-    const hasCardsPlayed = cardsPlayed.length > 0;
-    console.log("-----");
-    console.log(isDisabled ? 'isDisabled' : 'is not Disabled');
-    console.log(hasCardsPlayed ? 'hasCardsPlayed' : 'has not cards played');
-    console.log("-----");
-    return !hasCardsPlayed || isDisabled 
-}
 
 export default function CancelMovementsButton({ status }) {
-    const { idGame, idPlayer } = useContext(GameContext);
+    const {movCards, setMovCards, idGame, idPlayer } = useContext(GameContext);
+    
+    
+    function rollBackPlayedCards(idGame, idPlayer) {
+
+        cancelMovements(idGame);
+        const newMovCards = [...movCards];
+        for(const c in newMovCards){
+            if(newMovCards[c][1] === 'played'){
+                newMovCards[c][1] = 'nonplayed';
+            }
+        }
+        setMovCards(newMovCards);
+        
+        // TODO:
+        // 1) Actualizar las movCards con estado 'played' a 'noplayed'
+        //
+        // map(mvc.status => 'noplayed')
+        //
+        // 2) "Refrescar tablero" es automatico ya que se recibe REFRESH_BOARD por ws
+
+    }
+    function hideButton(status){
+        
+        const isDisabled = status === 'disabled';
+        let hasCardsPlayed = false;
+        console.log(movCards);
+        for(const mc in movCards) {
+            if(movCards[mc][1] === 'played') {
+                
+                hasCardsPlayed = true;
+            }
+        }
+        console.log("-----");
+        console.log(isDisabled ? 'isDisabled' : 'is not Disabled');
+        console.log(hasCardsPlayed ? 'hasCardsPlayed' : 'has not cards played');
+        console.log("-----");
+        return !hasCardsPlayed || isDisabled 
+    }
 
     return (
         <Col>
@@ -38,7 +54,7 @@ export default function CancelMovementsButton({ status }) {
                 onClick={() => rollBackPlayedCards(idGame, idPlayer)}
                 className="cancel-movements-button"
                 variant="danger"
-                disabled={hideButton(status, [])}
+                disabled={hideButton(status)}
             >
                 Cancelar Movimientos
             </Button>
